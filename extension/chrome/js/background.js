e435ce2c7d3bc0e797de35b2516b9e1add5a11cd
Argha Sarkar <argha@arghasarkar.co.uk>
2016-11-26 20:20:55 +0000

    feat(extension): Generating FSA Rating URL (#3)
    - Able to generate the FSA Rating URL
    
    - Breaking as FSA is HTTP and not HTTPS
    
    [QA]
    Has tests: No
    Reviewed by: Nil

--- extension/chrome/js/background.js
+++ extension/chrome/js/background.js
@@ -1,3 +1,11 @@
-FSA_HOST = "http://ratings.food.gov.uk/search/";
-FSA_HOST_SUFFIX = "json";
+var jq = document.createElement("script");
+
+jq.addEventListener("load", init); // pass my hoisted function
+jq.src = "js/vendors/jquery.js";
+document.querySelector("head").appendChild(jq);
+
+function init() {
+    FSA_HOST = "http://ratings.food.gov.uk/search/";
+    FSA_HOST_SUFFIX = "json";
+}
 
--- extension/chrome/js/content.js
+++ extension/chrome/js/content.js
@@ -1,3 +1,63 @@
-/**
- * Created by arghasarkar on 26/11/2016.
- */
+init();
+
+function init() {
+
+    FSA_HOST = "http://ratings.food.gov.uk/search/";
+    FSA_HOST_SUFFIX = "json";
+
+    takeaways = []
+    $( "div" ).filter(function( index ) {
+        return $( this ).attr( "class" ) == "o-tile c-restaurant";
+    }).each(function( index ) {
+
+        var postcode =  $( this ).find(
+            "[data-ft='restaurantDetailsAddress']"
+        ).html();
+
+        postcode = postcode.split(", ");
+        postcode = postcode[postcode.length - 1];
+
+        var rating = 0;
+
+        takeaways.push(
+            [
+               postcode,
+
+                $( this ).find(
+                    "[data-ft='restaurantDetailsName']"
+                ).html()
+
+                ,
+                rating
+            ]
+        );
+
+    });
+
+    for (var res in takeaways) {
+        var rating = getTakeawayRating(takeaways[res]);
+        console.log(takeaways[res]);
+    }
+}
+
+
+function getTakeawayRating(takeaway) {
+    var takeawayPostcode = takeaway[0];
+    var takeawayName = takeaway[1];
+
+    var ratingUrl = FSA_HOST + takeawayName + "/" + takeawayPostcode + "/" + FSA_HOST_SUFFIX;
+
+    console.log(ratingUrl);
+
+    var ratingJson = $.ajax({
+        url: ratingUrl,
+        success: function(data) {
+            console.log(data);
+        },
+        error: function () {
+            console.log("Error!");
+        }
+    });
+
+
+}
