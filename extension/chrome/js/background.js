355faaec5eaef5dd0068f4a0ef90ebd05228a14d
Argha Sarkar <argha.sarkar1994@gmail.com>
2016-11-26 21:13:28 +0000

    WIP: Background script can fetch ajax data

--- extension/chrome/js/background.js
+++ extension/chrome/js/background.js
@@ -9,3 +9,35 @@ function init() {
     FSA_HOST_SUFFIX = "json";
 }
 
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+        "from a content script:" + sender.tab.url :
+            "from the extension");
+        if (request) {
+            var rating = getTakeawayRating(request.postcode, request.name, function (data) {
+                console.log(data);
+                return data;
+            });
+            sendResponse({"rating": rating});
+        }
+
+    }
+);
+
+function getTakeawayRating(postcode, name, callback) {
+
+    var FSA_URL = FSA_HOST + name + "/" + postcode + "/" + FSA_HOST_SUFFIX;
+    console.log("URL: " + FSA_URL);
+
+    var ratingJson = $.ajax({
+        url: FSA_URL,
+        success: function(data) {
+            console.log(data);
+            callback(data);
+        },
+        error: function () {
+            console.log("Error!");
+        }
+    });
+}
